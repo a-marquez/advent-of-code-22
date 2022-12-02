23d686792aa5754eafa5afecdb498cec8c30a868
a-marquez <albert.marquez.2012@gmail.com>
2022-12-02 08:48:32 -0500

    Improve 01 syntax

--- 01/elf-calories.py
+++ 01/elf-calories.py
@@ -6,14 +6,14 @@ def sum(a, b):
 def cals_to_sums(cal_string):
   return functools.reduce(
     sum,
-    list(map(int, cal_string.split('\n')))
+    [*map(int, cal_string.split('\n'))]
   )
 
 if __name__ == '__main__':
   with open('input.txt') as input_file:
     input_content = input_file.read().strip()
     elf_calories = input_content.split('\n\n')
-    elf_calories = list(map(cals_to_sums, elf_calories))
+    elf_calories = [*map(cals_to_sums, elf_calories)]
     max_calories = max(elf_calories)
     print(f'max_calories: {max_calories}')
 
