bd53d4a9817d04a1a8f0d6ad09bae5a147ef52b4
a-marquez <albert.marquez.2012@gmail.com>
2022-12-09 16:08:27 -0500

    Tweaks

--- template.py
+++ template.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
   text_fname = sys.argv[0].split('.')[0] + '.txt'
   with open(text_fname) as file:
     content = file.read().strip()
-    test_content = ''
+    test_content = ''''''.strip()
 
