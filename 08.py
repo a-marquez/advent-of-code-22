a4b4cea44f9751e215804971e62bbd6293b3d12c
a-marquez <albert.marquez.2012@gmail.com>
2022-12-17 10:19:56 -0500

    Complete 08 part 2

--- 08.py
+++ 08.py
@@ -29,6 +29,56 @@ def visibility_east(grid, ri, ci):
   trees_compare = [grid[ri][xi] for xi in range(ci + 1, max_ci + 1)]
   return max(trees_compare) < tree_val
 
+def view_distance_north(grid, ri, ci):
+  if ri == 0: return 0
+
+  view_distance = 0
+  xi = ri
+  while xi != 0:
+    xi -= 1
+    view_distance += 1
+    compare_tree = grid[xi][ci]
+    if compare_tree >= grid[ri][ci]: break
+  return(view_distance)
+
+def view_distance_south(grid, ri, ci):
+  max_ri = len(grid) - 1
+  if ri == max_ri: return 0
+
+  view_distance = 0
+  yi = ri
+  while yi != max_ri:
+    yi += 1
+    view_distance += 1
+    compare_tree = grid[yi][ci]
+    if compare_tree >= grid[ri][ci]: break
+  return(view_distance)
+
+def view_distance_left(grid, ri, ci):
+  if ci == 0: return 0
+
+  view_distance = 0
+  xi = ci
+  while xi != 0:
+    xi -= 1
+    view_distance += 1
+    compare_tree = grid[ri][xi]
+    if compare_tree >= grid[ri][ci]: break
+  return(view_distance)
+
+def view_distance_right(grid, ri, ci):
+  max_ci = len(grid[0]) - 1
+  if ci == max_ci: return 0
+
+  view_distance = 0
+  xi = ci
+  while xi != max_ci:
+    xi += 1
+    view_distance += 1
+    compare_tree = grid[ri][xi]
+    if compare_tree >= grid[ri][ci]: break
+  return(view_distance)
+
 if __name__ == '__main__':
   text_fname = sys.argv[0].split('.')[0] + '.txt'
   with open(text_fname) as file:
@@ -64,3 +114,21 @@ if __name__ == '__main__':
     visibility_values = reduce(operator.add, visibility_values)
     visibility_values = reduce(operator.add, visibility_values)
     print(visibility_values)
+
+    trees_distances = defaultdict(dict)
+
+    for ri, row in enumerate(trees):
+      # print(ri, row)
+
+      for ci, col in enumerate(trees[ri]):
+        trees_distances[ri][ci] = [
+          view_distance_north(trees, ri, ci),
+          view_distance_south(trees, ri, ci),
+          view_distance_left(trees, ri, ci),
+          view_distance_right(trees, ri, ci)
+        ]
+        trees_distances[ri][ci] = reduce(operator.mul, trees_distances[ri][ci])
+
+    trees_distance_values = [*map(lambda row: [*row.values()], trees_distances.values())]
+    trees_distance_values = reduce(operator.add, trees_distance_values)
+    print(max(trees_distance_values))
